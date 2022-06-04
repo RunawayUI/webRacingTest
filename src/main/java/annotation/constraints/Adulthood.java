41497e0a63171fc818f1399fb52226d11d222f72
Anton Grafkov <tosha.grakof@yandex.ru>
2022-06-04 23:24:43 +0300

    валидатор для англ

--- src/main/java/annotation/constraints/Adulthood.java
+++ src/main/java/annotation/constraints/Adulthood.java
@@ -1,9 +1,17 @@
 package annotation.constraints;
 import org.jvnet.staxex.StAxSOAPBody;
 
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.CONSTRUCTOR, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = { })
 public @interface Adulthood {
     String message() default "Adulthood";
     int adulthoodAge() default 18;
-    Class<? extends StAxSOAPBody.Payload>[] payload() default { };
+    Class<? extends Payload>[] payload() default { };
     Class<?>[] groups() default { };
 }
--- src/main/java/annotation/constraints/EnglishLanguage.java
+++ src/main/java/annotation/constraints/EnglishLanguage.java
@@ -1,2 +1,21 @@
-package annotation.constraints;public @interface EnglishLanguage {
+package annotation.constraints;
+
+
+import validator.EnglishLanguageConstraintValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = { EnglishLanguageConstraintValidator.class })
+public @interface EnglishLanguage {
+    String message() default "EnglishLanguage";
+    boolean withNumbers() default true;
+    boolean withPunctuations() default true;
+    boolean withSpecSymbols() default true;
+    Class<? extends Payload>[] payload() default { };
+    Class<?>[] groups() default { };
 }
